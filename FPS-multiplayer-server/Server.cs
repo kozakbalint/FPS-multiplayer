f9a93c168d07510e4b96cc95b4382791610430ff
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2020-10-20 12:13:23 +0200

    Server Class Created

---
+++ FPS-multiplayer-server/Server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FPS_multiplayer_server
+{
+    class Server
+    {
+        public static int MaxPlayers { get; private set;}
+        public static int Port { get; private set;}
+
+        private static TcpListener tcpListener;
+
+        public void Start(int _maxPlayers, int _port)
+        {
+            MaxPlayers = _maxPlayers;
+            Port = _port;
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Server started on {Port}.");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+        }
+    }
+}
