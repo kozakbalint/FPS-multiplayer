04d1932c50699a855edacfa872fc48ace4797dec
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2020-10-20 12:51:55 +0200

    "InitializeServerData()" function added.

--- FPS-multiplayer-server/Server.cs
+++ FPS-multiplayer-server/Server.cs
@@ -10,25 +10,49 @@ namespace FPS_multiplayer_server
     {
         public static int MaxPlayers { get; private set;}
         public static int Port { get; private set;}
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
 
         private static TcpListener tcpListener;
 
-        public void Start(int _maxPlayers, int _port)
+        public static void Start(int _maxPlayers, int _port)
         {
             MaxPlayers = _maxPlayers;
             Port = _port;
+
+            Console.WriteLine("Server starting...");
+            InitializeServerData();
 
             tcpListener = new TcpListener(IPAddress.Any, Port);
             tcpListener.Start();
             tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
 
-            Console.WriteLine($"Server started on {Port}.");
+            Console.WriteLine($"Server started on port {Port}.");
         }
 
         private static void TCPConnectCallback(IAsyncResult _result)
         {
             TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
             tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                if (clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(_client);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{_client.Client.RemoteEndPoint} failed to connect: Server full!");
+        }
+
+        private static void InitializeServerData()
+        {
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                clients.Add(i, new Client(i));
+            }
         }
     }
 }
