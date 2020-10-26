21a4eb740da978c8363791386bc68a715b2ad844
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2020-10-26 23:53:52 +0100

    Client: Spawning player

--- FPS multiplayer/Assets/Scripts/Client.cs
+++ FPS multiplayer/Assets/Scripts/Client.cs
@@ -249,6 +249,7 @@ public class Client : MonoBehaviour
         packetHandlers = new Dictionary<int, PacketHandler>()
         {
             {(int)ServerPackets.welcome, ClientHandle.Welcome },
+            {(int)ServerPackets.spawnPlayer, ClientHandle.SpawnPlayer }
         };
         Debug.Log("Initialized packets.");
     }
